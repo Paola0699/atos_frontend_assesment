b16432481650dd1d4501b060078f5b5b7aaa9ec9
Paola Perez Valencia <39066877+Paola0699@users.noreply.github.com>
2021-12-20 15:58:15 -0600

    React-data-table component implemented, use this component to display the required data of each country on the system

--- src/App.js
+++ src/App.js
@@ -1,34 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
+
+const columns = [
+  {
+    name: 'Official name',
+    selector: row => row.name.official,
+    sortable: true,
+  },
+  {
+    name: 'Capital',
+    selector: row => row.capital,
+  },
+  {
+    name: 'Region',
+    selector: row => row.region,
+  },
+  {
+    name: 'Languages',
+    selector: row => <ul>{row.languages ? Object.values(row.languages).map((language, index) => <li key={index}>{language}</li>) : null}</ul>,
+  },
+  {
+    name: 'Population',
+    selector: row => row.population,
+  },
+  {
+    name: 'Flag',
+    selector: row => <img style={{ width: '50%' }} src={row.flags.png} />,
+  },
+];
 
 function App() {
 
   const [countriesData, setCountriesData] = useState([]);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setCountriesData(result);
-      },
-      (error) => {
-        console.log(error)
-      }
-    )
-  },[])
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setCountriesData(result);
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  }, [])
   return (
-   <div id="app">
-     {countriesData.map((country, index) => 
-       //console.log(country.name.nativeName)
-       //console.log(country.capital)
-       //console.log(country.region)
-       //console.log(country.languages)
-       //console.log(country.population)  
-       <img key={index} src={country.flags.png}/>
-     )}
-   </div>
+    <div id="app">
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">COUNTRIES</a>
+        </div>
+      </nav>  
+      <br/>
+      <div className="container">
+        <div className="row">
+          <h1>Countries List</h1>
+          <DataTable
+            columns={columns}
+            data={countriesData}
+            pagination={true}
+            paginationComponentOptions={{ rowsPerPageText: 'Rows per page:', rangeSeparatorText: 'from' }}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
