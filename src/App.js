a2b7e48ce00b9388ca556546d9418426f47d2d7e
Paola Perez Valencia <39066877+Paola0699@users.noreply.github.com>
2021-12-20 22:39:05 -0600

    Custom filter added, with select input and text input to select the column to search in and the input text to introduce the matching text

--- src/App.js
+++ src/App.js
@@ -47,7 +47,8 @@ function App() {
   const [selectedCountry, setSelectedCountry] = useState("");
   const [selectedCountryLanguages, setSelectedCountryLanguages] = useState("");
   const [countryData, setCountryData] = useState({});
-
+  const [selectedFilterOption, setSelectedFilterOption] = useState("Default");
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -55,6 +56,7 @@ function App() {
       .then(
         (result) => {
           setCountriesData(result);
+          setFilteredData(result);
         },
         (error) => {
           console.log(error)
@@ -78,6 +80,35 @@ function App() {
 
   }
 
+  const searchValues = (searchText) => {
+    if (searchText.length > 2) {
+      switch (selectedFilterOption) {
+        case 'Official Name':
+          const filteredCountriesByName = countriesData.filter(country => { return country.name.official.toLowerCase().includes(searchText.toLowerCase()) })
+          setFilteredData(filteredCountriesByName)
+          break;
+        case 'Capital':
+          const result = countriesData.filter(country => country.capital);
+          const filteredCountriesByCapital = result.filter(country => { return country.capital.toString().toLowerCase().includes(searchText.toLowerCase()) })
+          setFilteredData(filteredCountriesByCapital)
+          break;
+        case 'Region':
+          const filteredCountriesByRegion = countriesData.filter(country => { return country.region.toLowerCase().includes(searchText.toLowerCase()) })
+          setFilteredData(filteredCountriesByRegion)
+          break;
+        case 'Default':
+          const res = countriesData.filter(country => country.capital);
+          const filteredCountriesByAll = res.filter(country => {
+            return country.region.toLowerCase().includes(searchText.toLowerCase()) || country.name.official.toLowerCase().includes(searchText.toLowerCase()) || country.capital.toString().toLowerCase().includes(searchText.toLowerCase())
+          })
+          setFilteredData(filteredCountriesByAll)
+          break;
+        default:
+          setFilteredData(countriesData);
+      }
+    }
+  }
+
   return (
     <div id="app">
       <nav className="navbar navbar-dark bg-primary">
@@ -89,18 +120,32 @@ function App() {
       <div className="container">
         <div className="row">
           <h1>Countries List</h1>
+          <div className="row">
+            <div className="col"></div>
+            <div className="col">
+              <input onChange={e => searchValues(e.target.value)} className='form-control' type='text' placeholder="Search..." />
+            </div>
+            <div className="col">
+              <select className='form-control' onChange={e => { setSelectedFilterOption(e.target.value) }}>
+                <option defaultValue value="Default">Select an Option</option>
+                <option value="Official Name">Official Name</option>
+                <option value="Capital">Capital</option>
+                <option value="Region">Region</option>
+              </select>
+            </div>
+          </div>
           <DataTable
             columns={columns(selectCountry, setOpen, setSelectedCountryLanguages, setOpenModal)}
-            data={countriesData}
+            data={filteredData}
             pagination={true}
             paginationComponentOptions={{ rowsPerPageText: 'Rows per page:', rangeSeparatorText: 'from' }}
           />
-          <Modal open={open} onClose={() => setOpen(false)} center>
+          <Modal open={open} onClose={() => { setOpen(false); setSelectedCountry("") }} center>
             <h1>{selectedCountry}</h1>
             {countryData.extract_html ? parse(countryData.extract_html) : null}
           </Modal>
-          <Modal open={openModal} onClose={() => setOpenModal(false)} center>
-            <br/>
+          <Modal open={openModal} onClose={() => { setOpenModal(false);; setSelectedCountryLanguages("") }} center>
+            <br />
             <h1>Languages</h1>
             <ul>{selectedCountryLanguages ? Object.values(selectedCountryLanguages).sort().map((language, index) => <li key={index}>{language}</li>) : null}</ul>
           </Modal>
@@ -109,7 +154,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
-
-//<ul>{row.languages ? Object.values(row.languages).map((language, index) => <li key={index}>{language}</li>) : null}</ul>
